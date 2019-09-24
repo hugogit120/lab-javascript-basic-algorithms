fc9499a9897d616873abe9f177d4ef67cd34bc60
hugo <hugomercalibre@gmail.com>
2019-09-24 22:39:41 +0200

    my thirt commit

--- basic-algorithm.js
+++ basic-algorithm.js
@@ -5,78 +5,70 @@
 
 // Definir nombre del driver
 let hacker1 = prompt("Whats the driver's name?");
-console.log(`the drivers name is: ${hacker1}`);
+console.log(`The driver's name is: ${hacker1}`);
 
 
 // Definir nombre del navigator
-let hacker2 = prompt("Whats the navigator's name?");
-console.log(`the second navigator name is: ${hacker2}`);
+let hacker2 = prompt("What's the navigator's name?");
+console.log(`The navigator's name is: ${hacker2}`);
 
 
 // Calcular nombre mas largo
 function isLonger(hacker1, hacker2) {
   if (hacker1.length > hacker2.length) {
-    console.log(`the driver has the longest , it has: ${hacker1.length} caracters`);
+    console.log(`The driver has the longest name, it has: ${hacker1.length} characters`);
   } else if (hacker2.length > hacker1.length) {
-    console.log(`yo, navigator got the longest name, it has ${hacker2.length} caracters`);
+    console.log(`Yo, navigator got the longest name, it has ${hacker2.length} characters`);
   } else {
-    console.log(`wow, you both have equaly long names, ${hacker1.length}`);
+    console.log(`Wow, you both have equaly long names, ${hacker1.length} characters!!`);
   }
 }
 
 // Convertir a mayúsculas
 function nameCaps(name) {
-let answer = '';
-    for (let i = 0; i < name.length; i++) {
-      answer += `${name.charAt(i).toUpperCase()} `;
-  }
-  console.log(answer);
+  console.log(name.toUpperCase());
 }
 
 
 // Reversar nombre
 function reverse(name) {
-let answer = [];
-    for (let i = 0; i < name.length; i++) {
-      answer.push(name.charAt(i));
-  }
-  console.log(answer.reverse().join(""));
+  console.log(name.split("").reverse().join(""));
 }
 
 // Ordenar nombres lexicográficamente
 function lexiOrder(hacker1, hacker2) {
-  if  (hacker1.localeCompare(hacker2) > 0) {
-      console.log("The driver's name goes first");
-  } else if  (hacker1.localeCompare(hacker2) < 0) {
-        console.log("Yo, the navigator goes first definitely");
+  if (hacker1.localeCompare(hacker2) > 0) {
+    console.log("The driver's name goes first");
+  } else if (hacker1.localeCompare(hacker2) < 0) {
+    console.log("Yo, the navigator goes first definitely");
   } else {
-      console.log("What?! You both got the same name?");
+    console.log("What?! You both got the same name?");
   }
 }
 
 // Check Palindrome
 function palindrome(string) {
-    let originalString = [];
-  for (let i = 0; i < string.length; i++){
-    originalString.push(string.charAt(i).toLowerCase());
-  } 
-  let reverseString = [...originalString];
-  reverseString = reverseString.reverse();
-  if (originalString.toString().replace(/\W/g, '') === reverseString.toString().replace(/\W/g, '') ) {
-      console.log("its a palindrome")
-    } else {
-        console.log("it is not a palindrome")
-    }
+  if (string.split("").toString().toLowerCase().replace(/\W/g, '') === string.split("").reverse().toString().toLowerCase().replace(/\W/g, '')) {
+    console.log("its a palindrome")
+  } else {
+    console.log("it is not a palindrome")
+  }
 }
 
 // Lorem Ipsum Generator
 function countLoremIpsum(lorem) {
-let wordCounter = 0;
-    for (i = 0; i <= lorem.length; i++) {
-      if (lorem.charAt(i) == ' ' || lorem.charAt(i) == '\n') {
-        wordCounter++;
-    }
-  }
-  console.log(wordCounter + 1);
+  console.log(lorem.split(/\s+/).length);
 }
-countLoremIpsum(prompt("Escribe Lorem Ipsum"));
+
+// Count words
+function countEt(string) {
+    console.log(string.match(/\bet/g).length);
+}
+
+//isLonger(hacker1, hacker2);
+//nameCaps(hacker1);
+//reverse(hacker2);
+//lexiOrder(hacker1, hacker2);
+//palindrome(prompt("Introduce una frase para verificar si es un Palindrome"));
+//countLoremIpsum(prompt("Escribe un párrafo"));
+countEt(prompt());
